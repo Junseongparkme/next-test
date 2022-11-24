846affb7f7012280ad00e786de87e3599acf1112
Junseong Park <junseongpark.dev@gmail.com>
2022-11-24 18:18:12 +0900

    fix(config): update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'UXev Wiki',
-  url: 'https://.pages.dev',
+  url: 'https://.netlify.net',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
